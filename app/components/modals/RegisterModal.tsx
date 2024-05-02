9b8833d33ff5fa5a24829cc006b1f28a41617272
simona1707 <xlittleshinesx@gmail.com>
2024-05-02 19:51:41 +0300

    Add Categories UI

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -18,127 +18,136 @@ import Heading from "../Heading";
 import Button from "../Button";
 import Input from "../inputs/input";
 import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
-const RegisterModal= () => {
+const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
-  const { 
-    register, 
-    handleSubmit,
-    formState: {
-      errors,
-    },
+  const {
+      register,
+      handleSubmit,
+      formState: {
+          errors,
+      }
   } = useForm<FieldValues>({
-    defaultValues: {
-      name: '',
-      email: '',
-      password: ''
-    },
+      defaultValues: {
+          name:'',
+          email:'', 
+          password: ''
+      }
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    setIsLoading(true);
+      setIsLoading(true);
 
-    axios.post('/api/register', data)
-    .then(() => {
-      toast.success('Registered!');
-      registerModal.onClose();
-    })
-    .catch((error) => {
-      toast.error(error);
-    })
-    .finally(() => {
-      setIsLoading(false);
-    })
+      axios.post('/api/register', data)
+          .then(() => {
+              toast.success('Sign up successful!')
+              registerModal.onClose();
+              loginModal.onOpen();
+      })
+      .catch((error) => {
+          toast.error('Something went wrong');
+      })
+      .finally(() => {
+          setIsLoading(false);
+      })
   }
 
-  const onToggle = useCallback(() => {
-    registerModal.onClose();
-  }, [registerModal])
+  const toggle = useCallback(()=> {
+      registerModal.onClose();
+      loginModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
-    <div className="flex flex-col gap-4">
-      <Heading
-        title="Welcome to JourneyNest"
-        subtitle="Create an account!"
-      />
-      <Input
-        id="email"
-        label="Email"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id="name"
-        label="Name"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id="password"
-        label="Password"
-        type="password"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-    </div>
+      <div className="flex flex-col gap-4">
+          <Heading
+              title='Welcome to Airbnb'
+              subtitle='Create an account'             
+          />
+          <Input 
+          id="email"
+          label="Email"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+          />
+          <Input 
+          id="name"
+          label="Name"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+          />
+          <Input 
+          id="password"
+          type="password"
+          label="Password"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+          />
+      </div>
   )
 
   const footerContent = (
-    <div className="flex flex-col gap-4 mt-3">
-      <hr />
-      <Button 
-        outline 
-        label="Continue with Google"
-        icon={FcGoogle}
-        onClick={()=>signIn('google')} 
-      />
-      <Button 
-        outline 
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={()=>signIn('github')} 
-      />
-      <div 
-        className="
-          text-neutral-500 
-          text-center 
-          mt-4 
-          font-light
-        "
-      >
-        <p>Already have an account?
-          <span 
-            onClick={onToggle} 
-            className="
-              text-neutral-800
-              cursor-pointer 
-              hover:underline
-            "
-            > Log in</span>
-        </p>
+      <div className="flex flex-col gap-4 mt-3">
+          <hr />
+           <Button
+              outline
+              label = "Continue with Google"
+              icon={FcGoogle}
+              onClick={() =>signIn('google')}
+           />
+          <Button
+              outline
+              label = "Continue with Github"
+              icon={AiFillGithub}
+              onClick={() =>signIn('github')}
+           />
+           <div className="
+           text-neutral-500
+           text-center
+           mt-4
+           font-light
+           "
+           >
+          <div className="
+              flex flex-row items-center gap-2 justify-center">
+              <div>
+                  Already have an account?
+              </div>
+              <div 
+                  onClick={toggle}
+                  className="
+                      text-neutral-800
+                      cursor-pointer
+                      hover:underline
+                  "
+                  >
+                  Log in
+              </div>
+          </div>
+          </div>
       </div>
-    </div>
   )
 
-  return (
-    <Modal
-      disabled={isLoading}
-      isOpen={registerModal.isOpen}
-      title="Register"
-      actionLabel="Continue"
-      onClose={registerModal.onClose}
-      onSubmit={handleSubmit(onSubmit)}
-      body={bodyContent}
-      footer={footerContent}
-    />
+return (
+      <Modal
+          disabled={isLoading}
+          isOpen={registerModal.isOpen}
+          title="Register"
+          actionLabel="Continue"
+          onClose={registerModal.onClose}
+          onSubmit={handleSubmit(onSubmit)}
+          body={bodyContent}
+          footer={footerContent}
+      />
   );
 }
 
