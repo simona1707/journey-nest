8ea9998e68a3ec63f73f9a4a92cb01421b242315
simona1707 <xlittleshinesx@gmail.com>
2024-05-03 21:24:53 +0300

    add image upload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -164,6 +165,20 @@ const RentModal = () => {
                 value={bathroomCount}
                 onChange={(value) => setCustomValue('bathroomCount', value)}
                 />
+            </div>
+        )
+    }
+    if (step == STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                title="Add a photo of your place"
+                subtitle="Show guests what your place looks like!"
+                />
+              <ImageUpload
+                value={imageSrc}
+                onChange={(value) => setCustomValue('imageSrc', value)}
+              />
             </div>
         )
     }
