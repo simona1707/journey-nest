98365bc5739dcd73e91baa404c18d1dd4c450730
simona1707 <xlittleshinesx@gmail.com>
2024-05-03 20:19:10 +0300

    add location selection

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -45,10 +47,10 @@ const RentModal = () => {
     });
     const category = watch('category');
     const location = watch('location');
-    const guestCount = watch('guestCount');
-    const roomCount = watch('roomCount');
-    const bathroomCount = watch('bathroomCount');
-    const imageSrc = watch('imageSrc');
+
+    const Map = useMemo(()=> dynamic(()=> import('../Map'), { 
+        ssr:false
+    }),[location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id, value, {
@@ -109,12 +111,33 @@ const RentModal = () => {
             </div>
         </div>
     )
+    
+    if (step == STEPS.LOCATION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Where is your place located?"
+                    subtitle="Help guests find you!"
+                />
+
+
+                <CountrySelect 
+                    value={location}
+                    onChange={(value) => setCustomValue('location', value)}
+                />
+                <Map 
+                center={location?.latlng}
+                />
+            </div>
+
+        )
+    }
 
     return (
         <Modal 
         isOpen={rentModal.isOpen}
         onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
+        onSubmit={onNext}
         actionLabel={actionLabel}
         secondaryActionLabel={secondaryActionLabel}
         secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
