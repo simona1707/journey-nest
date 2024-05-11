58b4b324257928443161522052e608850511de85
simona1707 <xlittleshinesx@gmail.com>
2024-05-11 18:59:32 +0300

    Add price and  listing creation POST route

---
+++ app/api/listings/route.ts
@@ -0,0 +1,49 @@
+import prisma from "@/app/libs/prismadb"
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+    request: Request
+) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+    
+    const body = await request.json();
+    const {
+        category,
+        location,
+        guestCount,
+        roomCount,
+        bathroomCount,
+        imageSrc,
+        price,
+        title,
+        description
+    } = body;
+
+    Object.keys(body).forEach((value: any) => {
+        if (!body[value]) {
+            NextResponse.error();
+        }
+    });
+
+    const listing = await prisma.listing.create({
+        data: {
+            category,
+            guestCount,
+            roomCount,
+            bathroomCount,
+            imageSrc,
+            title,
+            description,
+            locationValue: location.value,
+            price: parseInt(price, 10),
+            userId: currentUser.id
+        }
+    });
+
+    return NextResponse.json(listing);
+}
