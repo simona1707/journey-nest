c0f27de5c0ddb9237d106a12e524815b491538ae
simona1707 <xlittleshinesx@gmail.com>
2024-05-30 21:20:33 +0300

    Add some icons

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,28 +1,33 @@
-'use client';
+import React from "react";
 
 interface MenuItemProps {
   onClick: () => void;
   label: string;
+  icon?: JSX.Element;
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({
   onClick,
-  label
+  label,
+  icon 
 }) => {
-  return ( 
-    <div 
-      onClick={onClick} 
+  return (
+    <div
+      onClick={onClick}
       className="
-      px-4 
-      py-3 
-      hover:bg-neutral-100 
+      px-4
+      py-3
+      hover:bg-neutral-100
       transition
       font-semibold
+      flex
+      items-center // Добавяме flex контейнер за центриране на иконката
       "
     >
+      {icon && <span className="mr-2">{icon}</span>} 
       {label}
     </div>
-   );
-}
- 
+  );
+};
+
 export default MenuItem;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -10,6 +10,7 @@ import { signOut } from 'next-auth/react';
 import { SafeUser } from "@/app/types";
 import useRentModal from "@/app/hooks/useRentModal";
 import { useRouter } from "next/navigation";
+import { FaBuilding, FaClipboardList, FaHeart, FaSignInAlt, FaSignOutAlt, FaSuitcase, FaUserPlus } from "react-icons/fa";
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
@@ -59,18 +60,19 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => router.push("/trips")} label="My trips" />
-                <MenuItem onClick={() => router.push("/favorites")} label="My favorites" />
-                <MenuItem onClick={() => router.push("/reservations")} label="My reservations" />
-                <MenuItem onClick={() => router.push("/properties")} label="My properties" />
-                <MenuItem onClick={rentModal.onOpen} label="Rent my home" />
+                <MenuItem onClick={() => router.push("/trips")} label="My Trips" icon={<FaSuitcase size={15} className="mr-2" />} />
+                <MenuItem onClick={() => router.push("/favorites")} label="My Favorites" icon={<FaHeart size={15} className="mr-2" />} />
+                <MenuItem onClick={() => router.push("/reservations")} label="My Reservations" icon={<FaClipboardList size={15} className="mr-2" />} />
+                <MenuItem onClick={() => router.push("/properties")} label="My Properties" icon={<FaBuilding size={15} className="mr-2" />} />
+                <MenuItem onClick={rentModal.onOpen} label="Rent My Home" icon={<MdOutlineAddHomeWork size={15} className="mr-2" />} />
                 <hr />
-                <MenuItem onClick={() => signOut()} label="Logout" />
+                <MenuItem onClick={() => signOut()} label="Logout" icon={<FaSignOutAlt size={15} className="mr-2" />} />
               </>
             ) : (
               <>
-                <MenuItem onClick={loginModal.onOpen} label="Login" />
-                <MenuItem onClick={registerModal.onOpen} label="Sign up" />
+           <MenuItem onClick={loginModal.onOpen} label="Login" icon={<FaSignInAlt size={15} />} />
+            <MenuItem onClick={registerModal.onOpen} label="Sign up" icon={<FaUserPlus size={15} />} />
+
               </>
             )}
           </div>
